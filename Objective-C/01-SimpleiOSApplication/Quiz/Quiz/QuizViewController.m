f7b2d2f5cd6da8dd28af4c7b2ecc78bc1d018bff
futantan <luckytantanfu@gmail.com>
2015-08-01 19:24:51 +0800

    完成第一行Quiz OC版本

---
+++ Objective-C/01-SimpleiOSApplication/Quiz/Quiz/QuizViewController.m
@@ -0,0 +1,56 @@
+//
+//  QuizViewController.m
+//  Quiz
+//
+//  Created by luckytantanfu on 8/1/15.
+//  Copyright (c) 2015 futantan. All rights reserved.
+//
+
+#import "QuizViewController.h"
+
+@interface QuizViewController ()
+
+@property (nonatomic) int currentQuestionIndex;
+
+@property (nonatomic, copy) NSArray *questions;
+@property (nonatomic, copy) NSArray *answers;
+
+@property (nonatomic, weak) IBOutlet UILabel *questionLabel;
+@property (nonatomic, weak) IBOutlet UILabel *answerLable;
+
+@end
+
+@implementation QuizViewController
+
+- (IBAction)showQuestion:(id)sender {
+    self.currentQuestionIndex++;
+    if (self.currentQuestionIndex == [self.questions count]) {
+        self.currentQuestionIndex = 0;
+    }
+    
+    NSString *question = self.questions[self.currentQuestionIndex];
+    self.questionLabel.text = question;
+    self.answerLable.text = @"???";
+}
+
+- (IBAction)showAnswer:(id)sender {
+    NSString *answer = self.answers[self.currentQuestionIndex];
+    self.answerLable.text = answer;
+}
+
+- (instancetype)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil {
+    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
+    
+    if (self) {
+        self.questions = @[@"From what is cognac made?",
+                           @"What is 7+7?",
+                           @"What is the capital of Vermont?"];
+        
+        self.answers = @[@"Grapes",
+                         @"14",
+                         @"Montpelier"];
+    }
+    return self;
+}
+
+@end
---
+++ Objective-C/01-SimpleiOSApplication/Quiz/Quiz/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Quiz
+//
+//  Created by luckytantanfu on 8/1/15.
+//  Copyright (c) 2015 futantan. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
