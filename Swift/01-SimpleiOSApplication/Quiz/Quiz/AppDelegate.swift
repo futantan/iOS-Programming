c353d7adabe6fff2bee89de191261e136d966674
futantan <luckytantanfu@gmail.com>
2015-08-01 22:14:17 +0800

    完成swift版本

--- Swift/01-SimpleiOSApplication/Quiz/Quiz/AppDelegate.swift
+++ Swift/01-SimpleiOSApplication/Quiz/Quiz/AppDelegate.swift
@@ -15,6 +15,10 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     func application(application: UIApplication, didFinishLaunchingWithOptions launchOptions: [NSObject: AnyObject]?) -> Bool {
         self.window = UIWindow(frame: UIScreen.mainScreen().bounds)
+        
+        let quizVC = QuizViewController()
+        self.window?.rootViewController = quizVC
+        
         self.window?.backgroundColor = UIColor.whiteColor()
         self.window?.makeKeyAndVisible()
         return true
