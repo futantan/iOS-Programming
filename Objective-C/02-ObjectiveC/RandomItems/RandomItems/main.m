9099cb7e0a6409edc693e560ed8018acf88af264
futantan <luckytantanfu@gmail.com>
2015-08-03 21:53:13 +0800

    init

---
+++ Objective-C/02-ObjectiveC/RandomItems/RandomItems/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  RandomItems
+//
+//  Created by luckytantanfu on 8/3/15.
+//  Copyright (c) 2015 futantan. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
