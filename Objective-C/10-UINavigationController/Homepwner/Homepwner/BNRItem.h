51adfa7152f0a2e0f29a9eee0e13b0ad3a5c9ff8
futantan <luckytantanfu@gmail.com>
2015-08-26 21:43:41 +0800

    增加navigation controller

---
+++ Objective-C/10-UINavigationController/Homepwner/Homepwner/BNRItem.h
@@ -0,0 +1,26 @@
+//
+//  BNRItem.h
+//  RandomItems
+//
+//  Created by luckytantanfu on 8/4/15.
+//  Copyright (c) 2015 futantan. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface BNRItem : NSObject
+
+@property (nonatomic, copy) NSString *itemName;
+@property (nonatomic, copy) NSString *serialNumber;
+@property (nonatomic) int valueInDollars;
+@property (nonatomic, readonly) NSDate *dateCreated;
+
++ (instancetype)randomItem;
+
+- (instancetype)initWithItemName:(NSString *)name
+                  valueInDollars:(int)value
+                    serialNumber:(NSString *)sNumber;
+
+- (instancetype)initWithItemName:(NSString *)name;
+
+@end
