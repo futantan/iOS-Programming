25f9e7edccd9c25ba49025cff7c6a21ca58a93d1
futantan <luckytantanfu@gmail.com>
2015-09-03 11:43:16 +0800

    init

---
+++ 15-IntroToAutoLayout/Homepwner/Homepwner/DetailViewController.h
@@ -0,0 +1,17 @@
+//
+//  DetailViewController.h
+//  Homepwner
+//
+//  Created by luckytantanfu on 8/26/15.
+//  Copyright (c) 2015 futantan. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@class BNRItem;
+
+@interface DetailViewController : UIViewController
+
+@property (nonatomic, strong) BNRItem *item;
+
+@end
